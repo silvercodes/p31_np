18919f439e46cfe03baf6577682cdb85998a985c
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-05-10 23:29:35 +0300

    add: intro

---
+++ 01_socket/01_socket/Program.cs
@@ -0,0 +1,33 @@
+﻿#region HW example
+//string[] files = Directory.GetFiles(@"C:\Users\ThinkPad\Desktop\img");
+//foreach (string item in files)
+//{
+//    Console.WriteLine(item);
+//}
+
+//Console.WriteLine("=============================");
+
+//async Task FileProcessingAsync()
+//{
+//    await Task.Run(() =>
+//    {
+//        Parallel.ForEach(files, path =>
+//        {
+//            // string content = File.ReadAllText(path);
+
+//            Thread.Sleep(2000);
+//            Console.WriteLine(path);
+//        });
+//    });
+//}
+
+//await FileProcessingAsync();
+#endregion
+
+
+
+
+
+
+
+
---
+++ 01_socket/02_server/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using System.Net.Sockets;
+
+const string serverIp = "127.0.0.1";
+const int port = 8080;
+
+
+using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+
+
+
+
